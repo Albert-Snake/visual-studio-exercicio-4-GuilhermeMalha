d811aa7d73296bfc5d5fff89877e2447259eeed3
Albert-Snake <60393951+Albert-Snake@users.noreply.github.com>
2020-02-11 15:10:07 +0000

    Add Mult e Menu
    Adicionei te um novo menu simples no programa e ainda adicionei mais uma função aritmética, a multiplicação.

---
+++ Execicio_GitHub_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Introduza o primeiro numero: ";
+    int um{};
+    cin >> um;
+    cout << "Introduza o segundo numero: ";
+    int dois{};
+    cin >> dois;
+    cout << "A soma de " << um << " + " << dois << " = " << (um + dois);
+}
+
